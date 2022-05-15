8f5cfdaef41125a8b084318a62782c2359657be8
Rafa <p52safer@uco.es>
2022-05-15 20:35:59 +0200

    Deleted extra points

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or any later version.
  *
- * Rx-Flappy. is distributed in the hope that it will be useful, but WITHOUT ANY
+ * Rx-Flappy is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * Rx-Flappy.. If not, see <https://www.gnu.org/licenses/>.
+ * Rx-Flappy. If not, see <https://www.gnu.org/licenses/>.
  */
 
 const gulp = require('gulp');
